d9e96cab0458c170f4ea08448eea41260c139700
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-07-07 11:53:00 -0300

    Version: Create new version at setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup
 
 
 setup(
-    name="redis-interval",
-    version="0.1",
+    name="redis_interval",
+    version="0.2.0",
     description="Extension of the python library for Redis with the Interval"
                 "sets extension of Redis",
     author="Gustavo Pantuza",
     author_email="gustavopantuza@gmail.com",
-    url="https://github.com/globo.com/redis-pyinterval",
+    url="https://github.com/globocom/redis-pyinterval",
+    download_url="https://github.com/globocom/redis-pyinterval/archive/0.2.0.tar.gz",
     install_requires=[
         "redis==2.10.5",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     packages=['redis_interval'],
+    classifiers=[],
 )
